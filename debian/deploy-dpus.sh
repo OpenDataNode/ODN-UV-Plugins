00d3f50c4f4b1ad853c8fc1c908ad4a7240e4a4d
Peter Klimo <peter.klimo@eea.sk>
2015-04-21 13:03:41 +0200

    curl command improved

--- debian/deploy-dpus.sh
+++ debian/deploy-dpus.sh
@@ -11,14 +11,14 @@ echo "---------------------------------------------------------------------"
     
 install_dpu() {
     dpu_file=$(ls $1)
-    
+
     echo -n "..installing $dpu_file: "
     outputfile="/tmp/dpu_out.out"
-    
+
     # fire cURL and wait until it finishes
-    curl --user $MASTER_USER:$MASTER_PASS --fail --silent --output $outputfile -X POST -H "Cache-Control: no-cache" -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F file=
+    curl --user $MASTER_USER:$MASTER_PASS --fail --silent --output $outputfile -X POST -H "Cache-Control: no-cache" -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F file=@$dpu_file $URL?force=true
     wait $!
-    
+
     # check if the installation went well
     outcontents=`cat $outputfile`
     echo $outcontents
